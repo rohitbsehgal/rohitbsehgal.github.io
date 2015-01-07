03c0536bc6d7507bba184ee124b58bcc2a01a08e
Rohit Sehgal <Rohit@varuns-iphone.local>
2015-01-07 01:58:25 -0600

    still working, but for now... heres the site
    UNDER CONSTRUCTION

--- js/functions.js
+++ js/functions.js
@@ -1,34 +1,12 @@
-var $el = $('.writer'),
-    txt = $el.text(),
-    txtLen = txt.length,
-    timeOut,
-    char = 0;
-
-$el.text('|');
-
-(function typeIt() {
-    var humanize = Math.round(150) + 30;
-    timeOut = setTimeout(function() {
-        char++;
-        var type = txt.substring(0, char);
-        $el.text(type + '|');
-        typeIt();
-
-        if (char == txtLen) {
-            $el.text($el.text().slice(0, -1)); // remove the '|'
-            clearTimeout(timeOut);
-        }
-
-    }, humanize);
-}());
-$('#example-basic').waypoint(function(direction) {
-  if(direction == 'down'){
-      document.getElementById("example-basic1").style.position = "fixed";
-      document.getElementById("example-basic1").style.display = "block";
-      document.getElementById("example-basic1").style.top = "0px";
-  }
-  if(direction == 'up'){
-      document.getElementById("example-basic1").style.display = "none";
-
-  }
+$('.header').particleground({
+  density: 4500,
+  minSpeedX: 0.5,
+  maxSpeedX: 1.0,
+  minSpeedY: 0.5,
+  maxSpeedY: 1.0,
+  proximity: 100,
+  parallaxMultiplier: 8
+});
+$('.content').appear(function(){
+  $('.header-links-fixed').css("position","fixed");
 });
