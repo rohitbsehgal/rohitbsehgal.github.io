d549517acc94a1e69ee417bbf43a67d7f8fa057e
Rohit Sehgal <Rohit@varuns-iphone.local>
2015-06-29 19:08:33 -0500

    update

--- functions.js
+++ functions.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
   $('.your-class').slick({
-
+    arrows: false
   });
 });
---
+++ some.js
@@ -0,0 +1,1 @@
+ for(var i = 0; i < 500; i++){var x = []; if(document.getElementsByTagName("a")[i].innerHTML.indexOf('@')){x.push(document.getElementsByTagName("a")[i].innerHTML)}}
